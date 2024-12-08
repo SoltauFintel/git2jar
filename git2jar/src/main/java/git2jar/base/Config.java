071fc10dd628779e37b98d2ed82fffaead40c117
Marcus Warm <warm@mwvb.de>
2024-12-08 12:18:45 +0100

    Work dirs

--- git2jar/src/main/java/git2jar/base/Config.java
+++ git2jar/src/main/java/git2jar/base/Config.java
@@ -9,48 +9,49 @@ import github.soltaufintel.amalia.web.config.AppConfig;
 public class Config {
     public static Config config;
 
-    /** base folder containing the other folders */
-    private final String basedir;
-    /** folder containing artifacts */
-    private final File repository;
-    /** work directory */
-    private final File work;
-    /** folder containing projects data */
+    /** persistent folder containing projects data */
     private final File projects;
+    /** persistent folder containing artifacts, used by WEB and SERVE */
+    private final File repository;
+    /** Work directory for getting tags from Git repositories. It's temporary but kept for improving performance. */
+    private final File tags;
+    /** Work directory for executing jobs. It's temporary and will be deleted after job execution or at app startup. */
+    private final File jobs;
     /** name of Docker image */
     private final String image;
     
     public Config(AppConfig c) {
-        basedir = c.get("basedir");
+        String basedir = c.get("basedir"); // base folder containing the other folders
         if (basedir == null || basedir.isEmpty()) {
             throw new RuntimeException("Config option 'basedir' is not set!");
         }
         Logger.debug("Config basedir: " + basedir);
-        repository = new File(basedir, c.get("repository", "repository"));
-        work = new File(basedir, c.get("work", "work"));
         projects = new File(basedir, c.get("projects", "projects"));
+        repository = new File(basedir, c.get("repository", "repository"));
+        tags = new File(basedir, c.get("tags", "tags"));
+        jobs = new File(basedir, c.get("jobs", "jobs"));
         image = c.get("image", "git2jar-jdk17");
 		if (image == null || image.isBlank()) {
 			throw new RuntimeException("Config option 'image' is not set!");
 		}
     }
     
-    public File getTagsWorkDir() {
-		return new File(work, "tags");
-    }
-
-    public File getJobsWorkDir() {
-    	return new File(work, "jobs");
-    }
-
-    public File getRepositoryDir() {
-        return repository;
-    }
-    
     public File getProjectsDir() {
         return projects;
     }
     
+    public File getRepositoryDir() {
+        return repository;
+    }
+
+    public File getTagsWorkDir() {
+		return tags;
+    }
+
+    public File getJobsWorkDir() {
+    	return jobs;
+    }
+
     public String getImage() {
     	return image;
     }
